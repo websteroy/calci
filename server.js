1f3a44e3864287444d32bf619201eda917f5b0d4
Elroy Webster D'Silva <elroy.dsilva@siemens.com>
2017-04-12 17:58:17 +0530

    Nodejs error fix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 app.set('view engine', 'html');
 app.use(express.static(__dirname + '/public'));
 
-var port = process.env.PORT || 6666;
+var port = process.env.PORT || 8080;
 app.listen(port, function () {
   console.log('Calci running on port '+ port + '!');
 });
